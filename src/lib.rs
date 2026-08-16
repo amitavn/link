cecfe8a3a69c10d349d1ff300e47b06cd4866236
Amitav Nott <58123848+anott03@users.noreply.github.com>
2026-08-16 17:08:29 -0500

    kv boilerplate

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,9 @@
-use axum::{routing::get, routing::post, Json, Router};
+use axum::{
+    extract::{Extension, Path},
+    routing::{get, post},
+    Json,
+    Router
+};
 use serde::Deserialize;
 use tower_service::Service;
 use worker::*;
@@ -8,20 +13,22 @@ pub struct ShortenRequest {
     url: String,
 }
 
-fn router() -> Router {
+fn router(kv: KvStore) -> Router {
     Router::new()
         .route("/", get(root))
         .route("/api/health", get(health))
         .route("/api/shorten", post(shorten))
+        .layer(Extension(kv))
 }
 
 #[event(fetch)]
 async fn fetch(
     req: HttpRequest,
-    _env: Env,
+    env: Env,
     _ctx: Context,
 ) -> Result<axum::http::Response<axum::body::Body>> {
-    Ok(router().call(req).await?)
+    let kv = env.kv("KV")?;
+    Ok(router(kv).call(req).await?)
 }
 
 pub async fn root() -> &'static str {
@@ -32,7 +39,8 @@ pub async fn health() -> axum::Json<serde_json::Value> {
     axum::Json(serde_json::json!({ "status": "ok" }))
 }
 
-pub async fn shorten(Json(req): Json<ShortenRequest>) -> axum::Json<serde_json::Value> {
+#[worker::send]
+pub async fn shorten(Extension(kv): Extension<KvStore>, Json(req): Json<ShortenRequest>) -> axum::Json<serde_json::Value> {
     // TODO
     axum::Json(serde_json::json!({ "url": req.url }))
 }
